a830a80467311c15e78de85c4a58fa11e365a05d
ZaPPaCCuDDa <scottteves08@gmail.com>
2025-02-03 00:05:26 -0500

    maybe works for pages

--- optimizer/src/main.tsx
+++ optimizer/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import '@mantine/core/styles.css'
-import { MantineProvider } from '@mantine/core'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import '@mantine/core/styles.css'; // Import Mantine styles
+import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
-        <App />
+      <App />
     </MantineProvider>
   </StrictMode>,
-)
+);
