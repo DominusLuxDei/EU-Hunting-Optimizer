0a0151281110ac25e34a5c758a7d6faefbc88e5e
mrkami1 <mashhood.anwar@gmail.com>
2025-02-04 15:33:30 -0500

    fixed ghpages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    css: {
-        modules: {
-            localsConvention: 'camelCase',
-        },
-    },
+    base: '/EU-Hunting-Optimizer'
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
+  base: '/EU-Hunting-Optimizer'
 });
